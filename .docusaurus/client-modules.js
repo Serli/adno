7f6540fa93b2856c5edf5995bd084e7e8d056714
SelligtomGit86 <thomas.delafaye@serli.com>
2022-09-05 09:03:41 +0200

    add gitignore & update package json

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/serli/Desktop/docusaurus/my-website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/serli/Desktop/docusaurus/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/serli/Desktop/docusaurus/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/serli/Desktop/docusaurus/my-website/src/css/custom.css'),
-];
