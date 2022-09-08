e40c4534f1e1c3ffdba1244b9db0150a4a725cbd
SelligtomGit86 <thomas.delafaye@serli.com>
2022-09-08 15:36:56 +0200

    update footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
 class Footer extends Component {
     constructor(props) {
@@ -11,9 +12,9 @@ class Footer extends Component {
 
             <footer className={this.props.location.pathname !== "/" || this.props.showFooter ? "" : "hide-footer"}>
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item"><a href="https://creativecommons.org/licenses/by-sa/4.0/" className="nav-link px-2 text-muted">Copyrights</a></li>
-                    <li className="nav-item"><a href="/legal" className="nav-link px-2 text-muted">Mentions Légales</a></li>
-                    <li className="nav-item"><a href="/cgu" className="nav-link px-2 text-muted">Conditions générales d'utilisation</a></li>
+                    <li><a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" className="nav-link px-2 text-muted">Copyrights</a></li>
+                    <li><Link to="/legal" className="nav-link px-2 text-muted">Mentions Légales</Link></li>
+                    <li><Link to="/cgu" className="nav-link px-2 text-muted">Conditions générales d'utilisation</Link></li>
                 </ul>
                 <p className="text-center text-muted"> &copy; ADNO {new Date().getFullYear()} - Espace Mendès France, Poitiers. Ce projet a été soutenu par le ministère de la Culture
                 </p>
