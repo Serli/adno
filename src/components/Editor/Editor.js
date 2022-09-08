8cac6a8435f38afad80812983aac18b5ea35e07d
SelligtomGit86 <thomas.delafaye@serli.com>
2022-09-08 14:20:51 +0200

    add security , check if a project exists before edit

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -5,7 +5,7 @@ import { withRouter } from "react-router";
 import Swal from "sweetalert2";
 
 // Import utils
-import { insertInLS } from "../../../Utils/utils";
+import { checkIfProjectExists, insertInLS } from "../../../Utils/utils";
 
 // Import Components
 import AnnotationCards from "./AnnotationCards/AnnotationCards";
@@ -23,149 +23,154 @@ class Editor extends Component {
 
     componentDidMount() {
 
-        if (this.props.match.params.id !== "" && this.props.match.params.id !== null && this.props.match.params.id !== undefined) {
-            let selected_project = JSON.parse(localStorage.getItem(this.props.match.params.id))
-
-            // Ajout d'un input pour le titre du projet
-            document.getElementById("project_name").value = selected_project.title
-
-            document.getElementById("project_name").addEventListener("change", function (e) {
-
-                selected_project.title = e.target.value
-                selected_project.last_update = new Date()
-
-                insertInLS(selected_project.id, JSON.stringify(selected_project))
-            })
-
-            // Ajout d'un input pour la description du projet
-            if (selected_project.description !== "") {
-                document.getElementById("project_desc").value = selected_project.description
-            } else {
-                document.getElementById("project_desc").placeholder = "Aucune description disponible pour ce projet"
-            }
-
-            document.getElementById("project_desc").addEventListener("change", function (e) {
-                selected_project.description = e.target.value
-                selected_project.last_update = new Date()
-
-                insertInLS(selected_project.id, JSON.stringify(selected_project))
-            })
-
-
-
-            // let selected_project = JSON.parse(localStorage.getItem(this.props.match.params.id))
-
-            if (JSON.parse(localStorage.getItem(this.props.match.params.id)).manifest_url) {
-                var viewer = OpenSeadragon({
-                    id: 'openseadragon1',
-                    tileSources: [JSON.parse(localStorage.getItem(this.props.match.params.id)).manifest_url],
-                    prefixUrl: 'https://openseadragon.github.io/openseadragon/images/'
-                });
-            } else {
-                var viewer = OpenSeadragon({
-                    id: 'openseadragon1',
-                    tileSources: {
-                        type: 'image',
-                        url: JSON.parse(localStorage.getItem(this.props.match.params.id)).img_url
-                    },
-                    prefixUrl: 'https://openseadragon.github.io/openseadragon/images/'
-                });
-            }
-
-
-
-
-            var anno = OpenSeadragon.Annotorious(viewer, {
-                locale: 'auto',
-                drawOnSingleClick: true,
-                allowEmpty: true,
-                disableEditor: false
-            });
-
-
-            // Find annotations from the localStorage in JSON format
-            var annotations = selected_project.id + "_annotations"
-            var annos = localStorage.getItem(annotations)
-
-            // Generate dataURI and load annotations into Annotorious
-            const dataURI = "data:application/json;base64," + btoa(unescape(encodeURIComponent(annos)));
-            anno.loadAnnotations(dataURI)
-
-
-            Annotorious.SelectorPack(anno);
-            Annotorious.BetterPolygon(anno);
-            Annotorious.Toolbar(anno, document.getElementById('toolbar-container'));
-
-
-
-            // Manage creation of new annotation
-            anno.on('createAnnotation', function (annotation) {
-                var project_annotations_id = selected_project.id + "_annotations";
-
-                var annotations = JSON.parse(localStorage.getItem(project_annotations_id))
-
-                if (annotations === undefined || annotations === null) {
-                    annotations = [
-                        annotation
-                    ]
-                } else {
-                    annotations.push(annotation)
-                }
-
-                // Update the last update date for the selected project
-                selected_project.last_update = new Date()
-                insertInLS(selected_project.id, JSON.stringify(selected_project))
-
-                // Update annotations linked to the selected project in the localStorage
-                insertInLS(project_annotations_id, JSON.stringify(annotations))
-
-                window.location.reload()
-
-            });
-
-            // Manage update of annotation
-            anno.on('updateAnnotation', function (upated_anno) {
-                let annotations = JSON.parse(localStorage.getItem(selected_project.id + "_annotations"))
-
-                let selected_anno = annotations.filter(anno => anno.id === upated_anno.id)[0]
-
-                Object.assign(selected_anno, upated_anno);
-
-                // Update the last update in the project
-                selected_project.last_update = new Date()
-                insertInLS(selected_project.id, JSON.stringify(selected_project))
-
-                // Save the updated annotation in the localStorage
-                insertInLS(selected_project.id + "_annotations", JSON.stringify(annotations))
-
-                window.location.reload()
-            });
-
-            anno.on('deleteAnnotation', function (del_anno) {
-                var annotations = JSON.parse(localStorage.getItem(selected_project.id + "_annotations"))
-
-
-                if (annotations && annotations.length === 1) {
-                    localStorage.removeItem(selected_project.id + "_annotations")
-                    window.location.reload()
-                } else {
-                    // Delete the annotation in the localStorage
-                    insertInLS(selected_project.id + "_annotations", JSON.stringify(annotations.filter(anno => anno.id !== del_anno.id)))
-                    window.location.reload()
-                }
-
-            })
-
-
+        if (!checkIfProjectExists(this.props.match.params.id)) {
+            this.props.history.push("/")
         } else {
-            document.getElementById("project_container").innerHTML = ""
-            Swal.fire({
-                title: "Aucun projet n'a été sélectionné",
-                showCancelButton: true,
-                showConfirmButton: false,
-                cancelButtonText: 'OK',
-                icon: 'warning',
-            })
+
+            if (this.props.match.params.id !== "" && this.props.match.params.id !== null && this.props.match.params.id !== undefined) {
+                let selected_project = JSON.parse(localStorage.getItem(this.props.match.params.id))
+
+                // Ajout d'un input pour le titre du projet
+                document.getElementById("project_name").value = selected_project.title
+
+                document.getElementById("project_name").addEventListener("change", function (e) {
+
+                    selected_project.title = e.target.value
+                    selected_project.last_update = new Date()
+
+                    insertInLS(selected_project.id, JSON.stringify(selected_project))
+                })
+
+                // Ajout d'un input pour la description du projet
+                if (selected_project.description !== "") {
+                    document.getElementById("project_desc").value = selected_project.description
+                } else {
+                    document.getElementById("project_desc").placeholder = "Aucune description disponible pour ce projet"
+                }
+
+                document.getElementById("project_desc").addEventListener("change", function (e) {
+                    selected_project.description = e.target.value
+                    selected_project.last_update = new Date()
+
+                    insertInLS(selected_project.id, JSON.stringify(selected_project))
+                })
+
+
+
+                // let selected_project = JSON.parse(localStorage.getItem(this.props.match.params.id))
+
+                if (JSON.parse(localStorage.getItem(this.props.match.params.id)).manifest_url) {
+                    var viewer = OpenSeadragon({
+                        id: 'openseadragon1',
+                        tileSources: [JSON.parse(localStorage.getItem(this.props.match.params.id)).manifest_url],
+                        prefixUrl: 'https://openseadragon.github.io/openseadragon/images/'
+                    });
+                } else {
+                    var viewer = OpenSeadragon({
+                        id: 'openseadragon1',
+                        tileSources: {
+                            type: 'image',
+                            url: JSON.parse(localStorage.getItem(this.props.match.params.id)).img_url
+                        },
+                        prefixUrl: 'https://openseadragon.github.io/openseadragon/images/'
+                    });
+                }
+
+
+
+
+                var anno = OpenSeadragon.Annotorious(viewer, {
+                    locale: 'auto',
+                    drawOnSingleClick: true,
+                    allowEmpty: true,
+                    disableEditor: false
+                });
+
+
+                // Find annotations from the localStorage in JSON format
+                var annotations = selected_project.id + "_annotations"
+                var annos = localStorage.getItem(annotations)
+
+                // Generate dataURI and load annotations into Annotorious
+                const dataURI = "data:application/json;base64," + btoa(unescape(encodeURIComponent(annos)));
+                anno.loadAnnotations(dataURI)
+
+
+                Annotorious.SelectorPack(anno);
+                Annotorious.BetterPolygon(anno);
+                Annotorious.Toolbar(anno, document.getElementById('toolbar-container'));
+
+
+
+                // Manage creation of new annotation
+                anno.on('createAnnotation', function (annotation) {
+                    var project_annotations_id = selected_project.id + "_annotations";
+
+                    var annotations = JSON.parse(localStorage.getItem(project_annotations_id))
+
+                    if (annotations === undefined || annotations === null) {
+                        annotations = [
+                            annotation
+                        ]
+                    } else {
+                        annotations.push(annotation)
+                    }
+
+                    // Update the last update date for the selected project
+                    selected_project.last_update = new Date()
+                    insertInLS(selected_project.id, JSON.stringify(selected_project))
+
+                    // Update annotations linked to the selected project in the localStorage
+                    insertInLS(project_annotations_id, JSON.stringify(annotations))
+
+                    window.location.reload()
+
+                });
+
+                // Manage update of annotation
+                anno.on('updateAnnotation', function (upated_anno) {
+                    let annotations = JSON.parse(localStorage.getItem(selected_project.id + "_annotations"))
+
+                    let selected_anno = annotations.filter(anno => anno.id === upated_anno.id)[0]
+
+                    Object.assign(selected_anno, upated_anno);
+
+                    // Update the last update in the project
+                    selected_project.last_update = new Date()
+                    insertInLS(selected_project.id, JSON.stringify(selected_project))
+
+                    // Save the updated annotation in the localStorage
+                    insertInLS(selected_project.id + "_annotations", JSON.stringify(annotations))
+
+                    window.location.reload()
+                });
+
+                anno.on('deleteAnnotation', function (del_anno) {
+                    var annotations = JSON.parse(localStorage.getItem(selected_project.id + "_annotations"))
+
+
+                    if (annotations && annotations.length === 1) {
+                        localStorage.removeItem(selected_project.id + "_annotations")
+                        window.location.reload()
+                    } else {
+                        // Delete the annotation in the localStorage
+                        insertInLS(selected_project.id + "_annotations", JSON.stringify(annotations.filter(anno => anno.id !== del_anno.id)))
+                        window.location.reload()
+                    }
+
+                })
+
+
+            } else {
+                document.getElementById("project_container").innerHTML = ""
+                Swal.fire({
+                    title: "Aucun projet n'a été sélectionné",
+                    showCancelButton: true,
+                    showConfirmButton: false,
+                    cancelButtonText: 'OK',
+                    icon: 'warning',
+                })
+            }
         }
     }
 
